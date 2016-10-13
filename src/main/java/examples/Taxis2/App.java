97f97dee0fc92f8a0605d6f69a43cf3b5836c40e
Raul Bustamante <rba76@alumnos.unican.es>
2016-10-13 11:26:43 +0200

    realizado commit ignorando carpeta target

---
+++ src/main/java/examples/Taxis2/App.java
@@ -0,0 +1,14 @@
+package examples.Taxis2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	RadioTaxiGUI.maini(args);
+    }
+}
